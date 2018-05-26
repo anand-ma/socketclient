3e8b90febb27042e574494094fcf416d8d509c91
Anand M A <anand_ma@directv.com>
2018-05-26 23:11:45 +0530

    update description

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -14,10 +14,10 @@ export class DemoComponent implements OnInit {
   constructor() {
     this.notifications = [];
     this.socket = io(this.url);
-
   }
+
   ngOnInit() {
-    this.socket.on('message', (data) => {
+    this.socket.on('message', data => {
       console.log(data);
       this.notifications.push(data);
     });
